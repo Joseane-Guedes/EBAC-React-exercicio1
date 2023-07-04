f0d33903040272f2526b3a71238379ff776b09f1
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-07-04 14:07:05 -0300

    add Forma 4

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 // Forma 1: Utilizando uma div com texto simples
-var shape1 = document.createElement('div');
+let shape1 = document.createElement('div');
 shape1.className = 'shape';
 shape1.textContent = 'Hello World - Forma 1';
 document.getElementById('container').appendChild(shape1);
 
 // Forma 2: Utilizando um elemento de cabeçalho h1
-var shape2 = document.createElement('h1');
+let shape2 = document.createElement('h1');
 shape2.textContent = 'Hello World - Forma 2';
 document.getElementById('container').appendChild(shape2);
 
 // Forma 3: Utilizando um parágrafo p
-var shape3 = document.createElement('p');
+let shape3 = document.createElement('p');
 shape3.textContent = 'Hello World - Forma 3';
 document.getElementById('container').appendChild(shape3);
+
+// Forma 4: Utilizando um span
+var shape4 = document.createElement('span');
+shape4.textContent = 'Hello World - Forma 4';
+document.getElementById('container').appendChild(shape4);
+
