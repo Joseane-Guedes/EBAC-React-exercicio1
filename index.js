84323e12de68bf4488409e95ac13b21d1f9edf1a
JOSEANE GUEDES <joseane_23@hotmail.com>
2023-07-04 13:45:57 -0300

    Add Ex forma 1

---
+++ index.js
@@ -0,0 +1,5 @@
+// Forma 1: Utilizando uma div com texto simples
+var shape1 = document.createElement('div');
+shape1.className = 'shape';
+shape1.textContent = 'Hello World - Forma 1';
+document.getElementById('container').appendChild(shape1);
